84124a09d6f944e1fce7b7e967512f58847667c2
TUTI254 <collotuti6@gmail.com>
2023-09-22 13:38:31 +0300

    feature(scope: user contoller and routes)
    what did i do?
    - created a user controller
    - created a user routes (CRUD)

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -19,3 +19,20 @@ export const isAuthenticated = async (
     return res.sendStatus(400);
   }
 };
+
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity._id") as string | undefined;
+    if (!currentUserId) return res.sendStatus(403);
+    if (currentUserId.toString() !== id) return res.sendStatus(403);
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
