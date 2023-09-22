94fee5fef8d49964832c392c30b45281653c6030
TUTI254 <collotuti6@gmail.com>
2023-09-22 09:50:50 +0300

    chore(scope: project setup )
    what did i do?
    - setup express server in inex.ts
    - add dependencies ( express,body-parser,compression, cookie-parse, cors )

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello there welcome to Muli !");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+// Create Express app
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+); //enable cors
+
+//Compress all routes
+app.use(compression());
+//Parse Cookie header and populate req.cookies
+// with an object keyed by the cookie names.
+app.use(cookieParser());
+//Parse incoming request bodies in a middleware before your handlers,
+// available under the req.body property.
+app.use(bodyParser.json());
+
+// Create HTTP server
+const server = http.createServer(app);
+
+server.listen(8000, () => {
+  console.log("Server is running on http://localhost:8000/ ");
+});
