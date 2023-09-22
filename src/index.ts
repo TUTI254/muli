961d2b866307a9cecb71d9364e62ab58a43273cb
TUTI254 <collotuti6@gmail.com>
2023-09-22 10:54:39 +0300

    chore(scope: project db setup)
     What did i do ?
        - added mongodb to the project
        - added mongoose to the project
        - added a connection to the database
        - added a schema for the user
        - remove the node_modules folder from the git repo

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
 
 // Create Express app
 const app = express();
@@ -29,3 +30,11 @@ const server = http.createServer(app);
 server.listen(8000, () => {
   console.log("Server is running on http://localhost:8000/ ");
 });
+
+const MONGO_URL =
+  "mongodb+srv://tuti254:tuti254@cluster0.qlzoqyq.mongodb.net/?retryWrites=true&w=majority";
+
+// Connect to MongoDB
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
